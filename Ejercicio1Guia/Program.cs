e616bef725b8b17c81f92d462d50f24b916e8c66
Rafael <rafaeladolforuiz@gmail.com>
2021-02-21 23:01:38 -0600

    Guia 6 ejercicio 1

---
+++ Ejercicio1Guia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio1Guia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.Title = "Tabla de multiplicar.";
+            Console.WriteLine("Inserte el número del cual desea saber la tabla de multiplicar del 1 al 10.");
+            Double num, res;
+            int n;
+            num = Double.Parse(Console.ReadLine());
+            for (n=0;n<=10;n=n+1)
+            {
+                res = num * n;
+                Console.WriteLine("\nEl valor escrito {" + num + "} multiplicado por: "+n+" es " +res);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.ReadKey();
+
+        }
+    }
+}
